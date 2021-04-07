df7634bf0afed652f0cf0178eeed9fdc5f4a563f
xjoker <wwj0110@hotmail.com>
2021-04-07 21:41:12 +0800

    修正Model路径错误问题

---
+++ Model/BaseResultModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Model
+{
+    public class BaseResultModel
+    {
+        public BaseResultModel(object data, int code = 200, string msg = "")
+        {
+            Code = code;
+            Msg = msg;
+            Data = data;
+        }
+
+        public BaseResultModel()
+        {
+        }
+
+        /// <summary>
+        ///     状态码
+        /// </summary>
+        public int Code { get; set; }
+
+        /// <summary>
+        ///     描述
+        /// </summary>
+        public string Msg { get; set; }
+
+        public object Data { get; set; }
+    }
+}
